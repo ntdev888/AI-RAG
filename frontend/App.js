c41d9b7d1453316833b37fba78710234cdb222ca
NT <123847387+ntdev888@users.noreply.github.com>
2024-05-07 11:23:41 +1200

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Chatbot from './chatbot';
+
+/* App.js*/
+
+function App() {
+  return (
+    <div><br></br>
+      <div><Chatbot />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
